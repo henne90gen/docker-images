230544b40f61ab3030894ad89bcf2f4d4d537970
Hendrik Müller <hendrik.mueller@fabmatics.com>
2024-09-05 13:13:39 +0200

    Add libadwaita to gtk image

--- gtk/test/main.cpp
+++ gtk/test/main.cpp
@@ -1,4 +1,5 @@
 #include <gtk/gtk.h>
+#include <adwaita.h>
 
 static void on_button_clicked(GtkWidget *widget, gpointer data)
 {
@@ -8,8 +9,8 @@ static void on_button_clicked(GtkWidget *widget, gpointer data)
 
 static void activate(GtkApplication *app, gpointer user_data)
 {
-  GtkWidget *window = gtk_application_window_new(app);
-  gtk_window_set_title(GTK_WINDOW(window), "Minimal GTK4 Example");
+  GtkWidget *window = adw_application_window_new(app);
+  gtk_window_set_title(GTK_WINDOW(window), "Minimal Example Application");
   gtk_window_set_default_size(GTK_WINDOW(window), 400, 300);
   GtkWidget *button = gtk_button_new_with_label("Click to Close");
   g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), window);
@@ -19,7 +20,7 @@ static void activate(GtkApplication *app, gpointer user_data)
 
 int main(int argc, char *argv[])
 {
-  GtkApplication *app = gtk_application_new("com.example.GtkApp", G_APPLICATION_DEFAULT_FLAGS);
+  AdwApplication *app = adw_application_new("com.example.App", G_APPLICATION_DEFAULT_FLAGS);
   g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
   return g_application_run(G_APPLICATION(app), argc, argv);
 }
